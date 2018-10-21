7e6f111694cd989c52a34de94368509e04d57058
王一扬 <newwangyiyang@163.com>
2018-10-21 15:49:16 +0800

    新增webpack配置项

--- .webpackrc.js
+++ .webpackrc.js
@@ -53,7 +53,7 @@ export default {
       viewportHeight: 1334, // (Number) The height of the viewport. 
       unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to. 
       viewportUnit: 'vw', // (String) Expected units. 
-      selectorBlackList: ['.ignore', '.hairlines', '::before', ':before', '::after', ':after'], // (Array) The selectors to ignore and leave as px. 
+      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px. 
       minPixelValue: 1, // (Number) Set the minimum pixel value to replace. 
       mediaQuery: false // (Boolean) Allow px to be converted in media queries. 
     }),
@@ -61,7 +61,9 @@ export default {
       utf8: false
     }),
     postcssCssnext({}),
-    postcssViewportUnits({}),
+    postcssViewportUnits({
+      filterRule: rule => rule.selector.indexOf('::after') === -1 && rule.selector.indexOf('::before') === -1 && rule.selector.indexOf(':after') === -1 && rule.selector.indexOf(':before') === -1
+    }),
     // preset: "advanced", 
     cssnano({
       autoprefixer: false, 
